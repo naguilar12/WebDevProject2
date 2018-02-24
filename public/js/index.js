10a3f971eb16ea32640f75a45556ff6eef640e4a
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-02-24 13:22:16 -0500

    Side navbar add

--- public/js/index.js
+++ public/js/index.js
@@ -2,7 +2,12 @@
 'use strict';
 
 function main() {
-    
+    $('#logo').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+    $('#logo_min').on('click',function () {
+        $('#sidebar').toggleClass('active');
+    });
 }
 
 $(document).ready(main);
