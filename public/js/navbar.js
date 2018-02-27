83ac725812c38798c28c8c65f811a92b31e869e2
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-02-27 12:41:02 -0500

    Navbar fixed options

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,11 +2,92 @@
 'use strict';
 
 function main() {
-    $('#logo').on('click', function () {
-        $('#sidebar').toggleClass('active');
+    $('#home-option').on('click', function () {
+        if(!$('#home-option').hasClass("active-item")){
+
+            $('#home-option').toggleClass("active-item");
+            $('#home-link').toggleClass("active-link");
+
+            if($('#my-stats-option').hasClass("active-item")){
+
+                $('#my-stats-option').toggleClass("active-item");
+                $('#my-stats-link').toggleClass("active-link");
+            }else if($('#challenge-option').hasClass("active-item")){
+
+                $('#challenge-option').toggleClass("active-item");
+                $('#challenge-link').toggleClass("active-link");
+            }else if($('#contact-option').hasClass("active-item")){
+
+                $('#contact-option').toggleClass("active-item");
+                $('#contact-link').toggleClass("active-link");
+            }
+        }
     });
-    $('#logo_min').on('click',function () {
-        $('#sidebar').toggleClass('active');
+
+    $('#my-stats-option').on('click', function () {
+        if(!$('#my-stats-option').hasClass("active-item")){
+
+            $('#my-stats-option').toggleClass("active-item");
+            $('#my-stats-link').toggleClass("active-link");
+
+            if($('#home-option').hasClass("active-item")){
+
+                $('#home-option').toggleClass("active-item");
+                $('#home-link').toggleClass("active-link");
+            }else if($('#challenge-option').hasClass("active-item")){
+
+                $('#challenge-option').toggleClass("active-item");
+                $('#challenge-link').toggleClass("active-link");
+            }else if($('#contact-option').hasClass("active-item")){
+
+                $('#contact-option').toggleClass("active-item");
+                $('#contact-link').toggleClass("active-link");
+            }
+        }
+    });
+
+    $('#challenge-option').on('click', function () {
+        if(!$('#challenge-option').hasClass("active-item")){
+
+            $('#challenge-option').toggleClass("active-item");
+            $('#challenge-link').toggleClass("active-link");
+
+            if($('#home-option').hasClass("active-item")){
+
+                $('#home-option').toggleClass("active-item");
+                $('#home-link').toggleClass("active-link");
+            }else if($('#my-stats-option').hasClass("active-item")){
+
+                $('#my-stats-option').toggleClass("active-item");
+                $('#my-stats-link').toggleClass("active-link");
+            }else if($('#contact-option').hasClass("active-item")){
+
+                $('#contact-option').toggleClass("active-item");
+                $('#contact-link').toggleClass("active-link");
+            }
+        }
+    });
+
+    $('#contact-option').on('click', function () {
+        if(!$('#contact-option').hasClass("active-item")){
+
+            $('#contact-option').toggleClass("active-item");
+            $('#contact-link').toggleClass("active-link");
+
+            if($('#home-option').hasClass("active-item")){
+
+                $('#home-option').toggleClass("active-item");
+                $('#home-link').toggleClass("active-link");
+            }else if($('#my-stats-option').hasClass("active-item")){
+
+                $('#my-stats-option').toggleClass("active-item");
+                $('#my-stats-link').toggleClass("active-link");
+            }else if($('#contact-option').hasClass("active-item")){
+
+                $('#challenge-option').toggleClass("active-item");
+                $('#challenge-link').toggleClass("active-link");
+            }
+        }
     });
 }
 
