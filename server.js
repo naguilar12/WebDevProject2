a6894908560f5bf3ffef35dceeb8e93cf072dcac
tf-venegas10 <tf.vengas10@uniandes.edu.co>
2018-02-28 13:16:26 -0500

    api and weight button

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const MongoClient  = require("mongodb").MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 const assert = require('assert');
 const request = require('request');
 const CRUD = require("./CRUD");
@@ -15,38 +15,38 @@ const dbName = 'nutrition';
 //Uses static directory "public"
 app.use(express.static("public"));
 
-app.get("/",(req,res) => res.sendfile("public","index.html"));
+app.get("/", (req, res) => res.sendfile("public", "index.html"));
 
-app.get("/API/food/:name", function (req, res){
+app.get("/API/food/:name", function (req, res) {
     // CALL API HERE
-    request("https://api.nal.usda.gov/ndb/search/?format=json&q="+req.params.name+"&max=25&offset=0&api_key=hLowbDVqOU42auJEBrZPL8tGUSbGd5ok91ficFr3",
-    function (error, response, body){
-        if (error){
-            console.log(error);
-        }
-        if (!error && response.statusCode == 200) {
-            res.send(JSON.parse(body).list.item);
-        }
-    })
-});
-app.get("/API/food/nutrition/:id", function (req, res){
-    // CALL API HERE
-    let url="https://api.nal.usda.gov/ndb/V2/reports?ndbno="+req.params.id+"&type=f&format=json&api_key=hLowbDVqOU42auJEBrZPL8tGUSbGd5ok91ficFr3";
-    request(url,
-        function (error, response, body){
-            if (error){
+    request("https://api.nal.usda.gov/ndb/search/?format=json&q=" + req.params.name + "&max=25&offset=0&api_key=hLowbDVqOU42auJEBrZPL8tGUSbGd5ok91ficFr3",
+        function (error, response, body) {
+            if (error) {
                 console.log(error);
             }
             if (!error && response.statusCode == 200) {
-                let food=JSON.parse(body).foods[0].food;
+                res.send(JSON.parse(body).list.item);
+            }
+        })
+});
+app.get("/API/food/nutrition/:id", function (req, res) {
+    // CALL API HERE
+    let url = "https://api.nal.usda.gov/ndb/V2/reports?ndbno=" + req.params.id + "&type=f&format=json&api_key=hLowbDVqOU42auJEBrZPL8tGUSbGd5ok91ficFr3";
+    request(url,
+        function (error, response, body) {
+            if (error) {
+                console.log(error);
+            }
+            if (!error && response.statusCode == 200) {
+                let food = JSON.parse(body).foods[0].food;
 
-                item={
-                    name:food.desc.name,
+                item = {
+                    name: food.desc.name,
                     kcals: food.nutrients[1].value,
                     protein: food.nutrients[3].value,
                     fat: food.nutrients[4].value,
                     carbohydrates: food.nutrients[6].value,
-                    fiber : food.nutrients[7].value
+                    fiber: food.nutrients[7].value
                 };
                 res.send(item);
             }
@@ -54,42 +54,105 @@ app.get("/API/food/nutrition/:id", function (req, res){
 
 });
 
-app.post("/API/myWeight/:userId/:value", function (req, res){
+app.post("/API/myWeight/:userId/:value", function (req, res) {
     //TODO: search db if user already has a document of weights add value, else create document
-    MongoClient.connect(DBurl, function(err, db) {
+    if (Number(req.params.userId) !== req.params.userId || Number(req.params.value) !== req.params.value) {
+        res.send([]);
+        console.log("wrong formulated API petition");
+        return;
+    }
+    MongoClient.connect(DBurl, function (err, db) {
         assert.equal(null, err);
         console.log("Connected successfully to server");
-        CRUD.insertWeight(db, function(weights) {
+        CRUD.insertWeight(db, function (weights) {
             db.close();
             res.send(weights);
         }, req.params.userId, req.params.value);
     });
 });
 
-app.get("/API/myWeight/:userId", function (req, res){
+app.get("/API/myWeight/:userId", function (req, res) {
     // search db if user already has a document of weights add value
-    MongoClient.connect(DBurl, function(err, db) {
+    if (Number(req.params.userId) !== req.params.userId) {
+        res.send([]);
+        return;
+    }
+    MongoClient.connect(DBurl, function (err, db) {
         assert.equal(null, err);
         console.log("Connected successfully to server");
 
-        CRUD.getWeights(db, function(weights) {
+        CRUD.getWeights(db, function (weights) {
             db.close();
             res.send(weights);
-        }, req.params.userId);
+        }, Number(req.params.userId));
     });
 });
 
-app.get("/API/myChallenge/:userId", function (req, res){
-    //TODO: search db if user already has a document of challenge add value
+app.get("/API/myChallenge/:userId", function (req, res) {
+    // search db if user already has a document of challenge returns last value
+    if (Number(req.params.userId) !== req.params.userId) {
+        res.send([]);
+        return;
+    }
+    MongoClient.connect(DBurl, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+
+        CRUD.getLastChallenge(db, function (weights) {
+            db.close();
+            res.send(weights);
+        }, Number(req.params.userId));
+    });
 });
-app.post("/API/myChallenge/:userId", function (req, res){
-    //TODO: search db if user already has a document of challenge add value
+app.post("/API/myChallenge/:userId", function (req, res) {
+    // search db if user already has a document of challenge add value
+    if (Number(req.params.userId) !== req.params.userId) {
+        res.send([]);
+        console.log("wrong formulated API petition");
+        return;
+    }
+
+    MongoClient.connect(DBurl, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        CRUD.insertChallenge(db, function (weights) {
+            db.close();
+            res.send(weights);
+        }, req.params.userId, req.body);
+    });
 });
-app.get("/API/myConsumption/:userId", function (req, res){
-    //TODO: search db if user already has a document of consumption add value
+app.get("/API/myConsumption/:userId", function (req, res) {
+    // search db if user already has a document of consumption returns last value
+    if (Number(req.params.userId) !== req.params.userId) {
+        res.send([]);
+        return;
+    }
+    MongoClient.connect(DBurl, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+
+        CRUD.getLastConsumption(db, function (weights) {
+            db.close();
+            res.send(weights);
+        }, Number(req.params.userId));
+    });
 });
-app.post("/API/myConsumption/:userId", function (req, res){
-    //TODO: search db if user already has a document of consumption add value
+app.post("/API/myConsumption/:userId", function (req, res) {
+    // search db if user already has a document of consumption add value
+    if (Number(req.params.userId) !== req.params.userId) {
+        res.send(Response.error());
+        console.log("wrong formulated API petition");
+        return;
+    }
+
+    MongoClient.connect(DBurl, function (err, db) {
+        assert.equal(null, err);
+        console.log("Connected successfully to server");
+        CRUD.insertChallenge(db, function (weights) {
+            db.close();
+            res.send(weights);
+        }, req.params.userId, req.body);
+    });
 });
 
 
@@ -98,7 +161,4 @@ app.listen(process.env.PORT || 80, () => {
 });
 
 
-MongoClient.connect(DBurl, function(err, db) {
-    assert.equal(null, err);
-    console.log("success");
-});
+
