5785e4f38e57fc0a943adba9b6cf9682879e0d21
jmalovera10 <jma.lovera10@uniandes.edu.co>
2018-02-28 14:16:52 -0500

    Stats html add

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+/*global $*/
+"use strict";
+
+function main() {
+    $("#home-option").click();
+    $("#home-link").click();
+}
+
+$(document).ready(main);
---
+++ public/js/stats.js
@@ -0,0 +1,9 @@
+/*global $*/
+"use strict";
+
+function main() {
+    $("#my-stats-option").click();
+    $("#my-stats-link").click();
+}
+
+$(document).ready(main);
